f64923a559695fb83381e97b3c40d62b6c3cf7c5
Francisco Gomes <95197081+fgsj0203@users.noreply.github.com>
2025-10-08 23:24:34 +0000

    feat: inserting calculate power card, modify type data and comparate values of two cards.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -25,21 +25,26 @@ int main() {
   char state_a[2];
   char card_code_a[5];
   char name_city_a[20];
-  int population_city_a;
+  unsigned long int population_city_a;
   float area_a, pib_city_a;
   int number_points_touristics_a;
   float density_population_a;
   float pib_per_capita_a;
+  long int power_card_a;
+  unsigned int counter_power_card_a = 0;
+  
   //------------ End data head card A ------------
 
   char state_b[2];
   char card_code_b[5];
   char name_city_b[20];
-  int population_city_b;
+  unsigned long int population_city_b;
   float area_b, pib_city_b;
   int number_points_touristics_b;
   float density_population_b;
   float pib_per_capita_b;
+  long int power_card_b;
+  unsigned int counter_power_card_b = 0;
 
   //------------ End data head card B ------------
 
@@ -65,7 +70,7 @@ int main() {
   printf("Help data input: input with number of amount population city A. number type integer\n");
   printf("Suporte ao usuário: digite o número da população da cidade A. Tipo número: inteiro\n");
   printf("Enter with a population A: \n");
-  scanf("%d", &population_city_a);
+  scanf("%ld", &population_city_a);
 
   printf("Help data input: input a value of area city in KM². Type data: float\n");
   printf("Suporte ao usuário: entre com um valor da área da cidade em KM². Tipo de dado: número decimal\n");
@@ -106,7 +111,7 @@ int main() {
   printf("Help data input: input with number of amount population city B. number type integer\n");
   printf("Suporte ao usuário: digite o número da população da cidade B. Tipo número: inteiro\n");
   printf("Enter with a population B: \n");
-  scanf("%d", &population_city_b);
+  scanf("%ld", &population_city_b);
 
   printf("Help data input: input a value of area city in KM². Type data: float\n");
   printf("Suporte ao usuário: entre com um valor da área da cidade em KM². Tipo de dado: número decimal\n");
@@ -122,7 +127,6 @@ int main() {
   printf("Suporte ao usuário: entre com o número de pontos turísticos da cidade B. Tipo de dado: inteiro\n");
   printf("Enter with a amount points touristics in city B: \n");
   scanf("%d", &number_points_touristics_b);
-  printf("\n");
 
   printf("-------------- END DATA CARD [B] ---------------\n");
 
@@ -136,13 +140,16 @@ int main() {
   pib_per_capita_b = pib_city_b / population_city_b;  
 
   //-------------------------------------------------------------------------------------
+  // Calculating power of cards
+  power_card_a = population_city_a + (float)area_a + (float)pib_city_a + (int)number_points_touristics_a + (float)pib_per_capita_a;
+  power_card_b = population_city_b + (float)area_b + (float)pib_city_b + (int)number_points_touristics_b + (float)pib_per_capita_b;
 
   // Área para exibição dos dados da cidade
   printf("----------- Dados da carta A -----------\n");
   printf("State: %s\n", state_a);
   printf("Card code A: %s%s\n", state_a, card_code_a);
   printf("Name of city: %s\n", name_city_a);
-  printf("Population A: %d\n", population_city_a);
+  printf("Population A: %ld\n", population_city_a);
   printf("Area city A: %.2f\n", area_a);
   printf("PIB city A: %.2f\n", pib_city_a);
   printf("Number points touristics A: %d\n", number_points_touristics_a);
@@ -153,7 +160,7 @@ int main() {
   printf("State: %s\n", state_b);
   printf("Card code B: %s%s\n", state_b, card_code_b);
   printf("Name of city: %s\n", name_city_b);
-  printf("Population B: %d\n", population_city_b);
+  printf("Population B: %ld\n", population_city_b);
   printf("Area city B: %.2f\n", area_b);
   printf("PIB city B: %.2f\n", pib_city_b);
   printf("Number points touristics B: %d\n", number_points_touristics_b);
@@ -161,6 +168,90 @@ int main() {
   printf("The PIB per capita of population B: %.2f Reais\n", pib_per_capita_b);
 
   //---------------------------------------------------------------------------------------------------
+  // Comparating values of two cards
+    printf("Comparating of two cards\n");
+    if (population_city_a > population_city_b)
+    {
+      printf("Population: \n");
+      counter_power_card_a++;
+      printf("Card 1 win (1)\n");
+      printf("Carta 1 venceu (1)\n");
+    }
+    else
+    {
+      printf("Population: \n");
+      counter_power_card_b++;
+      printf("Card 2 win (0)\n");
+      printf("Carta 2 venceu (0)\n");
+    }
+    if (area_a > area_b)
+    {
+      printf("Area: \n");
+      counter_power_card_a++;
+      printf("Card 1 win (1)\n");
+      printf("Carta 1 venceu (1)\n");
+    }
+    else
+    {
+      printf("Area: \n");
+      counter_power_card_b++;
+      printf("Card 2 win (0)\n");
+      printf("Carta 2 venceu (0)\n");
+    }
+    if (pib_city_a > pib_city_b)
+    {
+      printf("PIB: \n");
+      counter_power_card_a++;
+      printf("Card 1 win (1)\n");
+      printf("Carta 1 venceu (1)\n");
+    }
+    else
+    {
+      printf("PIB: \n");
+      counter_power_card_b++;
+      printf("Card 2 win (0)\n");
+      printf("Carta 2 venceu (0)\n");
+    }
+    if (number_points_touristics_a > number_points_touristics_b)
+    {
+      printf("Point Touristics: \n");
+      counter_power_card_a++;
+      printf("Card 1 win (1)\n");
+      printf("Carta 1 venceu (1)\n");
+    }
+    else
+    {
+      printf("Point Touristics: \n");
+      counter_power_card_b++;
+      printf("Card 2 win (0)\n");
+      printf("Carta 2 venceu (0)\n");
+    }
+    if (density_population_a > density_population_b)
+    {
+      printf("Density Population: \n");
+      counter_power_card_b++;
+      printf("Card 2 win (0)\n");
+      printf("Carta 2 venceu (0)\n");
+    }
+    else
+    {
+      printf("Density Population: \n");
+      counter_power_card_a++;
+      printf("Card 1 win (1)\n");
+      printf("Carta 1 venceu (1)\n");
+    }
+    if (power_card_a > power_card_b)
+    {
+      printf("Super power: \n");
+      printf("Card 1 win (1)\n");
+      printf("Carta 1 venceu (1)\n");
+    }
+    else
+    {
+      printf("Super power: \n");
+      printf("Card 2 win (0)\n");
+      printf("Carta 2 venceu (0)\n");
+    }
 
 
 return 0;
